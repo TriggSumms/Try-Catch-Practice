f1a4827e0eaebe597e52ba40c6a0be44ffa37e2d
Trigg Summs <stsumms@gmail.com>
2020-08-27 18:47:57 -0500

    Close, but im not yet able to figure out the Contact List

---
+++ Contact.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace TryCatch
+{
+
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get { return $"{FirstName}{LastName}"; }
+        }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+    }
+
+}
---
+++ TryCatch.cs
@@ -0,0 +1,36 @@
+ using System.Collections.Generic;
+ using System;
+
+ namespace TryCatch
+ {
+     public class AddressBook
+     {
+
+       Dictionary<string, Contact> Contacts = new Dictionary<string, Contact>();
+//double?
+
+/*         private List<Employee> _employees = new List<Employee>();
+        public string Name { get; set; }
+
+        public void AddEmployee(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            return _employees[id];
+        }
+    } */
+
+
+
+
+
+
+
+
+
+     }
+
+ }
